768e19bfea0e3d4438c9d66bab98a0a56a82bc73
bonsaiheart <109669915+bonsaiheart@users.noreply.github.com>
2023-01-04 15:53:39 -0500

    Added directories to lessen confusion Added chart from matplotlib Added previous closing prices for "matching days on list - today"

--- main.py
+++ main.py
@@ -6,41 +6,73 @@ import pandas as pd
 import yfinance as yf
 import chkMarketConditions
 import operations
-
-# dailyCSVentries = glob.glob(r'dataOutput\SPY_Daily_CSVs\*.csv')
-#
-# data = []
-#
-# for csv in dailyCSVentries:
-#     df = pd.read_csv(csv)
-#     data.append(df)
-#
-# bigframe = pd.concat(data, sort=False, ignore_index=True)
-# bigframe.set_index('maturitydate',  inplace=True)
-# bigframe.columns = pd.to_datetime(bigframe.columns, format='%m/%d/%y')
-# bigframe.index = pd.to_datetime(bigframe.index, format='%m/%d/%y')
-# # bigframe.columns = bigframe.columns.sort_values(ascending=False)
-# # bigframe.index = bigframe.index.sort_values(ascending=False)
-#
-# bigframe = bigframe.reindex(sorted(bigframe.index))
-# bigframe = bigframe.transpose()
-# bigframe = bigframe.reindex(sorted(bigframe.index))
-#
-#
-# listofmatchingdates = [pd.to_datetime(c, format='%m/%d/%y').strftime("%Y/%m/%d") for c in bigframe.columns if c in bigframe.index]
-# def changeDatestoStrings(i):
-#     x = i.replace('/', '-')
-#     return str(x)
-#
-# listofmatchingdatesAsSTRINGS = [changeDatestoStrings(i) for i in listofmatchingdates]
-# bigframe.index = pd.to_datetime(bigframe.index, format='%m/%d/%y').strftime("%m/%d/%y")
-# bigframe.columns = pd.to_datetime(bigframe.columns, format='%m/%d/%y').strftime("%m/%d/%y")
-#
-# bigframe = bigframe.fillna("N/A")
-#
-#
+import matplotlib.pyplot as plt
+import retrieve_mp_data
 
 
-getHistoryListStartWithOldest("spy", listofmatchingdatesAsSTRINGS[0])
+retrieve_mp_data
+
+dailyCSVentries = glob.glob(r'dataOutput\SPY_Daily_CSVs\*.csv')
+
+data = []
+
+
+for csv in dailyCSVentries:
+    df = pd.read_csv(csv)
+    data.append(df)
+
+bigframe = pd.concat(data, sort=False, ignore_index=True)
+bigframe.set_index('maturitydate',  inplace=True)
+bigframe.columns = pd.to_datetime(bigframe.columns, format='%m/%d/%y')
+bigframe.index = pd.to_datetime(bigframe.index, format='%m/%d/%y')
+
+
+
+bigframe = bigframe.reindex(sorted(bigframe.index))
+bigframe = bigframe.transpose()
+bigframe = bigframe.reindex(sorted(bigframe.index))
+
+
+listofmatchingdates = [pd.to_datetime(c, format='%m/%d/%y').strftime("%Y/%m/%d") for c in bigframe.columns if c in bigframe.index]
+today_datetime = datetime.today().strftime("%Y/%m/%d")
+listofmatchingdates.remove(today_datetime)
+
+def swapBackslashDash(i):
+    x = i.replace('/', '-')
+    return (x)
+
+listofmatchingdateswithBackslash = [swapBackslashDash(i) for i in listofmatchingdates]
+bigframe.index = pd.to_datetime(bigframe.index, format='%m/%d/%y').strftime("%m/%d/%y")
+bigframe.columns = pd.to_datetime(bigframe.columns, format='%m/%d/%y').strftime("%m/%d/%y")
+
+
+
+
+
+
+spy = operations.RetrieveHistoricalData("SPY")
+
+for a in listofmatchingdateswithBackslash :
+    a = f"{a}"
+    b = (datetime.strptime(a,'%Y-%m-%d').strftime("%m/%d/%y"))
+
+    closingprice = '{:,.2f}'.format(spy.populateClosingPrice(a))
+
+
+    indexvalue = (bigframe.columns.get_loc(b)) + 1
+    columnvalue = (bigframe.index.get_loc(b))
+
+    bigframe.iat[columnvalue,indexvalue] = closingprice
 
 bigframe.to_csv("dataoutput/maxpain.csv")
+
+bigframe=bigframe.astype(float)
+bigframe.info()
+chart1 = bigframe.plot(alpha=.3, loglog=False, lw=3, colormap='prism', marker='.', linestyle="dotted",markersize=10, title='SPY Heuristic staggered MaxPain')
+chart1.set_xlabel("Exp. Date")
+chart1.set_ylabel("Price $$$")
+print(bigframe.index)
+
+plt.legend(title="Date Gen.")
+plt.show()
+bigframe = bigframe.fillna("n/a")
--- retrieve_mp_data.py
+++ retrieve_mp_data.py
@@ -43,6 +43,7 @@ data_list = []
 data_dict = {"maturitydate": dateGenerated}
 todayinweirdformat = date.today()
 today = pd.to_datetime(todayinweirdformat, infer_datetime_format='%m/%d/%y').strftime("%m-%d-%y")
+todayasYYMMDD = todayinweirdformat.strftime("%Y_%m_%d")
 
 
 
@@ -54,11 +55,13 @@ for option in all_options:
 
 
     if maturitydate != repeat_date_check_list[-1]:
-        maxpain = str(driver.find_element(By.XPATH,  "/html/body/app-root/div/div[1]/div/app-options/div/div[10]/div/app-maxpain/table/caption/b[2]").get_attribute("innerHTML")).removeprefix("Max Pain ")
+        maxpainstring = (driver.find_element(By.XPATH,  "/html/body/app-root/div/div[1]/div/app-options/div/div[10]/div/app-maxpain/table/caption/b[2]").get_attribute("innerHTML")).removeprefix("Max Pain $")
+        maxpain = '{:,.2f}'.format(float(maxpainstring))
+        print(maxpain)
         data_list.append(maxpain)
         data_dict[maturitydate] = maxpain
         repeat_date_check_list.append(maturitydate)
-
+        print(maxpain)
     else:
         print("Error: Webdriver has timed out.")
         data_dict = ""
@@ -67,7 +70,7 @@ for option in all_options:
 driver.close()
 print(data_dict)
 if error == 0:
-    with open(f"dataOutput/SPY_Daily_CSVs/{today} SPY MaxPain.csv", 'w') as file:
+    with open(f"dataOutput/SPY_Daily_CSVs/{todayasYYMMDD} SPY MaxPain.csv", 'w') as file:
         writer = csv.DictWriter(file, data_dict.keys())
         writer.writeheader()
         writer.writerow(data_dict)
