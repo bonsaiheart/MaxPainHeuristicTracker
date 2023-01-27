933839fdb043c2eacc1db6ccd9c0d8c5268b985d
bonsaiheart <nhuff89@gmail.com>
2023-01-27 07:20:58 -0500

    V1.1 - working on adding new OI calculations from raw data adding indication of weekly close above/below(by %?) MP

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import maxpain_to_csvandchart
+
+import openinterestapi as oi
 
 
+tickerlist = open("Inputs/tickers_to_track.txt", "r")
 
-maxpain_to_csvandchart()
+for ticker in tickerlist.readlines():
+    ticker = ticker.strip().upper()
+    oi.retrive_oi_data(ticker)
+    oi.write_oi_data_to_csv(ticker)
+    oi.get_highest_oi_PC(ticker)
+
+
+tickerlist.close()
--- openinterestapi.py
+++ openinterestapi.py
@@ -1,7 +1,5 @@
-from datetime import datetime
-from bs4 import BeautifulSoup
-import requests
-from pandas import pandas
+
+import os
 import wget
 from pathlib import Path
 import pandas as pd
@@ -16,31 +14,105 @@ def retrive_oi_data(ticker):
     output_dir2 = Path(f"dataOutput/{ticker}_daily_OI")
     output_dir.mkdir(mode=0o755,parents=True, exist_ok=True)
     output_dir2.mkdir(mode=0o755,parents=True, exist_ok=True)
-    wget.download(f"https://marketdata.theocc.com/series-search?symbolType=O&symbol={ticker}", f"tempdata/{ticker}/{today}_oi_dl.csv")
+    if os.path.exists(f"tempdata/{ticker}/{today}_oi_dl.txt"):
+        os.remove(f"tempdata/{ticker}/{today}_oi_dl.txt")
+    if os.path.exists(f"tempdata/{ticker}/{today}_oi_dl_UNALTERED.txt"):
+        os.remove(f"tempdata/{ticker}/{today}_oi_dl_UNALTERED.txt")
+    wget.download(f"https://marketdata.theocc.com/series-search?symbolType=O&symbol={ticker}", f"tempdata/{ticker}/{today}_oi_dl.txt")
+    wget.download(f"https://marketdata.theocc.com/series-search?symbolType=O&symbol={ticker}", f"tempdata/{ticker}/{today}_oi_dl_UNALTERED.txt")
+
 
 def write_oi_data_to_csv(ticker):
     ticker = ticker.upper()
-    oicsv = pd.read_csv(f"tempdata/{ticker}/{today}_oi_dl.csv")
-    oicsv.columns = oicsv.iloc[3]
-    oicsv = oicsv.iloc[3:]
+    with open(f"tempdata/{ticker}/{today}_oi_dl.txt", "r+") as infile:
+        txt = infile.readlines()[6:]
+        txt[0] = txt[0].replace("Position Limit", "PositionLimit")
+        txt[0] = txt[0].replace("year", "Year")
+        infile.seek(0)
+        infile.writelines(txt)
+        infile.truncate()
 
-    oicsv.columns = ["ProductSymbol",	"year",	"Month",	"Day"	,"Integer"	,"Dec"	,"C/P"	,"Call",	"Put"	,"Position" ,"Limit"]
+    with open(f"tempdata/{ticker}/{today}_oi_dl.txt", "r+") as infile:
+        txt = infile.read().replace('\t\t','\t')
+        infile.seek(0)
+        infile.writelines(txt)
+        infile.truncate()
+
+    oicsv = pd.read_table(f"tempdata/{ticker}/{today}_oi_dl.txt")
+    oicsv.drop(oicsv.columns[6], axis=1, inplace=True)
+    oicsv.drop(oicsv.columns[8], axis=1, inplace=True)
+    oicsv.drop(oicsv.columns[8], axis=1, inplace=True)
+    oicsv.drop(oicsv.columns[0], axis=1, inplace=True)
+    dec_as_float = oicsv["Dec"]
+    float = dec_as_float/1000
+
+    for value in oicsv["Integer"]:
+        oicsv["Strike"] = oicsv["Integer"] + float
+    oicsv.drop("Integer", axis=1, inplace=True)
+    oicsv.drop("Dec", axis=1, inplace=True)
+    oicsv['ExpDate'] = oicsv['Year'].astype(str) + oicsv['Month'].astype(str).str.zfill(2) + oicsv['Day'].astype(str).str.zfill(2)
+    oicsv["ExpDate"] = pd.to_datetime(oicsv["ExpDate"], format="%Y%m%d")
+    oicsv.drop(["Year"], axis=1,inplace=True)
+    oicsv.drop(["Month"], axis=1,inplace=True)
+    oicsv.drop(["Day"], axis=1,inplace=True)
+    oicsv = oicsv.reindex(columns =["ExpDate","Strike","Call","Put"])
+    oicsv.to_csv(f"dataOutput/{ticker}_daily_OI/{ticker}_{today}.csv",index_label="index")
 
 
-    oicsv.to_csv(f"dataOutput/{ticker}_daily_OI/{today}.csv")
 
 def get_highest_oi_PC(ticker):
-    df = pd.read_csv(f"dataOutput/{ticker}_daily_OI/{today}.csv")
-    df["Period"] = df['year'].astype(str) + "-" + df["Duration"]
-    print()
+    ticker = ticker.upper()
+    df = pd.read_csv(f"dataOutput/{ticker}_daily_OI/{ticker}_{today}.csv",index_col=["index"])
+    print(df)
+    calls_list = []
+    highest_oi_calls_dict = {}
+    puts_list = []
+    highest_oi_puts_dict = {}
 
-retrive_oi_data("ROKU")
-write_oi_data_to_csv("roku")
-### I want the highest OI put/call, max pain, ...
+    for value in df["Call"]:
+        calls_list.append(value)
+    calls_list.sort(reverse=True)
+    highcall_indexvalue = df[df['Call'] == calls_list[0]].index[0]
+###TODO I think i want a chart taht has STRIKE, then above/below, the top OI.  There fore, I should probalby use lists instead of dicts.
 
-# for converting unix to date
-# ts = int('1284101485')
-#
-# # if you encounter a "year is out of range" error the timestamp
-# # may be in milliseconds, try `ts /= 1000` in that case
-# print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+    for value in df["Put"]:
+        puts_list.append(value)
+    puts_list.sort(reverse=True)
+    highput_indexvalue = df[df['Put'] == puts_list[0]].index[0]
+
+
+
+    for oi in calls_list[:5]:
+        highcall_indexvalue = df[df['Call'] == oi].index[0]
+        strike = df.at[highcall_indexvalue, "Strike"]
+        highest_oi_calls_dict[oi] = strike
+
+    for oi in puts_list[:5]:
+        highput_indexvalue = df[df['Put'] == oi].index[0]
+        strike = df.at[highput_indexvalue, "Strike"]
+        highest_oi_puts_dict[oi] = strike
+
+
+    print(f"Calls ={highest_oi_calls_dict}")
+    print(f"Puts ={highest_oi_puts_dict}")
+    output_dir = Path(f"dataOutput/{ticker}")
+    output_dir.mkdir(mode=0o755,parents=True, exist_ok=True)
+
+    #TODO make this write a datafrome, discover a suitable data vis. format.
+    for key, value in highest_oi_calls_dict.items():
+        df.loc[key] = value
+        print(highest_oi_calls_dict)
+
+    # with open(f"dataOutput/{ticker}/highest_oi_strikes.txt", "a") as outfile:
+    #     calls_df = pd.DataFrame.from_dict(highest_oi_calls_dict)
+    #     print(calls_df)
+    #     puts_df = pd.DataFrame.from_dict(highest_oi_puts_dict)
+    #     oi_df = pd.puts_df.concat(calls_df)
+    #     outfile.write(oi_df)
+
+
+
+
+
+
+#TODO collect OI data by "date fetched", collect into one file?
